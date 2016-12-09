e4e243a0e0fde9c4f5b2634c799c21b7d8be48e0
jonathan gerber <jlgerber@gmail.com>
2016-12-09 07:03:31 -0800

    split out palette setting into function in main

--- main.cpp
+++ main.cpp
@@ -3,11 +3,7 @@
 #include <QStyleFactory>
 #include <QStyle>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    auto *style = QStyleFactory::create("Fusion");
-    QApplication::setStyle(style);
+void setDarkPalette(QApplication& app) {
     QPalette darkPalette;
     darkPalette.setColor(QPalette::Window, QColor(53,53,53));
     darkPalette.setColor(QPalette::WindowText, Qt::white);
@@ -23,9 +19,16 @@ int main(int argc, char *argv[])
     darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
     darkPalette.setColor(QPalette::HighlightedText, Qt::black);
 
-    a.setPalette(darkPalette);
+    app.setPalette(darkPalette);
+    app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+}
 
-    a.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    auto *style = QStyleFactory::create("Fusion");
+    QApplication::setStyle(style);
+    setDarkPalette(a);
     Startup startup;
     startup.show();
 
